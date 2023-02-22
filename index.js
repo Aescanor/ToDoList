5b4c676113b8c26ddfc0254164945b431d331a75
Æscanor <113015001+Aescanor@users.noreply.github.com>
2023-02-22 16:56:38 +0100

    function delete all v2

--- index.js
+++ index.js
@@ -29,17 +29,17 @@ addTaskButton.addEventListener("click", () => {
     let newLi = document.createElement("li")
     newLi.classList = "task"
     newLi.innerHTML = `${dataInput}`
-   
+
     // 2.b - Ajout dans le DOM :
     taskList.appendChild(newLi)
 
-    
+
     // 3 - Status:
-    
+
     // 3.a - inProgress :
     let inProgress = document.createElement("input")
     inProgress.type = "checkbox"
-    inProgress.className ="inProgress"
+    inProgress.className = "inProgress"
     inProgress.id = "status"
     inProgress.name = "status"
     inProgress.value = "InProgress"
@@ -50,69 +50,69 @@ addTaskButton.addEventListener("click", () => {
     //3.b - Ajout dans le DOM :
     newLi.appendChild(inProgress)
     newLi.appendChild(textInProgress)
-    
+
     // 3.c - Achieved: 
     let Achieved = document.createElement("input")
     Achieved.type = "checkbox"
-    Achieved.className ="achieved"
+    Achieved.className = "achieved"
     Achieved.id = "status"
     Achieved.name = "status"
     Achieved.value = "achieved"
 
     // création du texte de l'input :
     let textAchieved = document.createTextNode("Terminé")
-    
+
     // 3.d - Ajout dans le DOM :
     newLi.appendChild(Achieved)
     newLi.appendChild(textAchieved)
-    
+
     // 4-Gestion des états des checkboxs:
-    
+
     // Déclaration de la variable de l'input checkbox: 
     let labelInprogress = document.createElement("label")
-    
+
     // Déclaration de la variable de l'input checkbox: 
     let labelAchieved = document.createElement("label")
-    
+
     // function change()
     inProgress.addEventListener("change", () => {
-        
+
         if (inProgress.checked = true) {
             // disabled the other checkbox :
             Achieved.checked = false
-            
+
             // création du label :
             labelInprogress.textContent = "en cours"
             labelInprogress.style.color = "orange"
-            
+
             newLi.appendChild(labelInprogress)
             labelAchieved.remove()
-            
+
         } else {
             labelInprogress.remove()
             labelAchieved.remove()
         }
-        
+
     })
-    
+
     // function change()
     Achieved.addEventListener("change", () => {
-        
+
         if (Achieved.checked = true) {
             inProgress.checked = false
-            
+
             // création du label :
             labelAchieved.textContent = "terminé"
             labelAchieved.style.color = "green"
-            
+
             newLi.appendChild(labelAchieved)
             labelInprogress.remove()
-            
+
         } else {
             labelInprogress.remove()
             labelAchieved.remove()
         }
-        
+
     })
 
     // 4 - Suppression 
@@ -120,7 +120,7 @@ addTaskButton.addEventListener("click", () => {
     // 4.a - Création du bouton suppr:
     let btnSuppr = document.createElement("button")
     // Attribution d'une classe :
-    btnSuppr.className ="btnSuppr"
+    btnSuppr.className = "btnSuppr"
     // Attribution du contenu:
     btnSuppr.textContent = "Supprimer"
 
@@ -133,30 +133,30 @@ addTaskButton.addEventListener("click", () => {
         newLi.remove()
         btnSuppr.remove()
     })
-    
+
     // 5.Clear taskInput :
     taskInput.value = ""
 
     // 6 - Button btnSupprAll:
 
-        // 6.a déclaration du btn :
-        let btnSupprAll = document.querySelector("button.btnSupprAll")
+    // 6.a déclaration du btn :
+    let btnSupprAll = document.querySelector("button.btnSupprAll")
 
-        // 6.b function toggle du btn :
-        function togglebtnSupprAll(){
-    
-            return(newLi ? btnSupprAll.style.visibility ="visible" : btnSupprAll.style.visibility ="hidden")
-    
-        }
-        togglebtnSupprAll()
-    
-    
-        // 6.c - function de suppression de tous les newli (tasks)
-    
-        btnSupprAll.addEventListener("click", () =>{
+    // 6.b function toggle du btn :
+    function togglebtnSupprAll() {
 
-            let allTasks = document.querySelectorAll(...newLi)
+        return (newLi ? btnSupprAll.style.visibility = "visible" : btnSupprAll.style.visibility = "hidden")
 
-            allTasks.remove()
-        })
+    }
+    togglebtnSupprAll()
+
+
+    // 6.c - function de suppression de tous les newli (tasks)
+
+    btnSupprAll.addEventListener("click", () => {
+
+        taskList.innerHTML =""
+        btnSupprAll.remove()
+        
+    })
 })
